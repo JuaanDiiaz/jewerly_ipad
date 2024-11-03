5aab28b1f791ca02668983060e82b20107df1564
Thelma Díaz Marmolejo <thelmucha@Thelmas-MacBook-Air.local>
2024-11-03 15:25:19 -0600

    Add/MyFirstCommit

---
+++ android/app/src/main/kotlin/com/example/p_a_jewerly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p_a_jewerly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:p_a_jewerly/screens/home_screen.dart';
