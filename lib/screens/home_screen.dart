f0e152663fb4655303e3f17d35faa4d99df2e246
JuaanDiiaz <minijordan22@gmail.com>
2024-11-03 22:31:08 -0600

    Add/SideBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math show pi;
 import 'package:collapsible_sidebar/collapsible_sidebar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,176 +10,133 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late List<CollapsibleItem> _items;
-  late String _headline;
-  AssetImage _avatarImg = AssetImage('assets/man.png');
+  late Widget _currentWidget;
 
   @override
   void initState() {
     super.initState();
     _items = _generateItems;
-    _headline = _items.firstWhere((item) => item.isSelected).text;
+    _currentWidget = Center(child: Text('Inicio'),);
   }
 
     List<CollapsibleItem> get _generateItems {
     return [
       CollapsibleItem(
-          text: 'Dashboard',
+          text: 'Sales',
           icon: Icons.assessment,
-          onPressed: () => setState(() => _headline = 'DashBoard'),
-          onHold: () => ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: const Text("Dashboard"))),
+          onPressed: () => setState((){
+            _currentWidget = Center(child:Text('Sale'));
+          }),
           isSelected: true,
           subItems: [
             CollapsibleItem(
-              text: 'Menu',
-              icon: Icons.menu_book,
-              onPressed: () => setState(() => _headline = 'Menu'),
-              onHold: () => ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: const Text("Menu"))),
+              text: 'New sale',
+              icon: Icons.add,
+              onPressed: () => setState((){
+                _currentWidget = Center(child:Text('New sale'));
+              }),
               isSelected: true,
             ),
             CollapsibleItem(
-                text: 'Shop',
-                iconImage: AssetImage("assets/shop_icon.png"),
-                icon: Icons.ac_unit,
-                onPressed: () => setState(() => _headline = 'Shop'),
-                onHold: () => ScaffoldMessenger.of(context)
-                    .showSnackBar(SnackBar(content: const Text("Shop"))),
+                text: 'Sales list',
+                icon: Icons.list,
+                onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Sales list'));
+              }),
                 isSelected: true,
-                subItems: [
-                  CollapsibleItem(
-                    text: 'Cart',
-                    icon: Icons.shopping_cart,
-                    onPressed: () => setState(() => _headline = 'Cart'),
-                    onHold: () => ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: const Text("Cart"))),
-                    isSelected: true,
-                  )
-                ]),
+              ),
           ]),
       CollapsibleItem(
-        text: 'Search',
-        icon: Icons.search,
-        onPressed: () => setState(() => _headline = 'Search'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Search"))),
+        text: 'Inventory',
+        icon: Icons.list_alt_sharp,
+        onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Inventory'));
+              }),
+        subItems: [
+          CollapsibleItem(
+            text: 'Movements', 
+            icon: Icons.directions,
+            onPressed: () => setState(() {
+              
+            }),
+          ),
+          CollapsibleItem(
+            text: 'Add movement', 
+            icon: Icons.add,
+            onPressed: () => setState(() {
+              
+            }),
+          ),
+          CollapsibleItem(
+            text: 'Physical count', 
+            icon: Icons.list_rounded,
+            onPressed: () => setState(() {
+              
+            }),
+          ),
+        ]
       ),
       CollapsibleItem(
-        text: 'Notifications',
-        icon: Icons.notifications,
-        onPressed: () => setState(() => _headline = 'Notifications'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Notifications"))),
+        text: 'Prices',
+        icon: Icons.money,
+        onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Prices'));
+              }),
+      ),
+      CollapsibleItem(
+        text: 'Purchasess',
+        icon: Icons.person,
+        onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Purchasess'));
+              }),
+        subItems: [
+          CollapsibleItem(
+            text: 'Purchase order', 
+            icon: Icons.edit_document,
+            onPressed: () => setState(() {
+              
+            }),
+          ),
+          CollapsibleItem(
+            text: 'Purchases items', 
+            icon: Icons.list_rounded,
+            onPressed: () => setState(() {
+              
+            }),
+          ),
+        ]
+      ),
+      CollapsibleItem(
+        text: 'Customers',
+        icon: Icons.person_2,
+        onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Customers'));
+              }),
+        subItems: [
+          CollapsibleItem(
+            text: 'Payments', 
+            icon: Icons.money_sharp,
+            onPressed: () => setState(() {
+              
+            }),
+          ),
+        ]
+      ),
+      CollapsibleItem(
+        text: 'Products',
+        icon: Icons.article,
+        onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Products'));
+              }),
       ),
       CollapsibleItem(
         text: 'Settings',
         icon: Icons.settings,
-        onPressed: () => setState(() => _headline = 'Settings'),
+        onPressed: () => setState((){
+                _currentWidget = Center(child:Text('Settings'));
+              }),
         onHold: () => ScaffoldMessenger.of(context)
             .showSnackBar(SnackBar(content: const Text("Settings"))),
-      ),
-      CollapsibleItem(
-        text: 'Alarm',
-        icon: Icons.access_alarm,
-        onPressed: () => setState(() => _headline = 'Alarm'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Alarm"))),
-      ),
-      CollapsibleItem(
-        text: 'Eco',
-        icon: Icons.eco,
-        onPressed: () => setState(() => _headline = 'Eco'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Eco"))),
-      ),
-      CollapsibleItem(
-        text: 'Event',
-        icon: Icons.event,
-        onPressed: () => setState(() => _headline = 'Event'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Event"))),
-      ),
-      CollapsibleItem(
-        text: 'No Icon',
-        onPressed: () => setState(() => _headline = 'No Icon'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("No Icon"))),
-      ),
-      CollapsibleItem(
-        text: 'Email',
-        icon: Icons.email,
-        onPressed: () => setState(() => _headline = 'Email'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Email"))),
-      ),
-      CollapsibleItem(
-          text: 'News',
-          iconImage: NetworkImage(
-              "https://cdn-icons-png.flaticon.com/512/330/330703.png"),
-          onPressed: () => setState(() => _headline = 'News'),
-          onHold: () => ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: const Text("News"))),
-          subItems: [
-            CollapsibleItem(
-              text: 'Old News',
-              icon: Icons.elderly,
-              onPressed: () => setState(() => _headline = 'Old News'),
-              onHold: () => ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: const Text("Old News"))),
-            ),
-            CollapsibleItem(
-                text: 'Current News',
-                icon: Icons.yard_outlined,
-                onPressed: () => setState(() => _headline = 'Current News'),
-                onHold: () => ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: const Text("Current News"))),
-                subItems: [
-                  CollapsibleItem(
-                    text: 'News 1',
-                    icon: Icons.one_k,
-                    onPressed: () => setState(() => _headline = 'News 1'),
-                    onHold: () => ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: const Text("News 1"))),
-                  ),
-                  CollapsibleItem(
-                      text: 'News 2',
-                      icon: Icons.two_k,
-                      onPressed: () => setState(() => _headline = 'News 2'),
-                      onHold: () => ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: const Text("News 2"))),
-                      subItems: [
-                        CollapsibleItem(
-                          text: 'News 2 Detail',
-                          icon: Icons.two_k_outlined,
-                          onPressed: () =>
-                              setState(() => _headline = 'News 2 Detail'),
-                          onHold: () => ScaffoldMessenger.of(context)
-                              .showSnackBar(SnackBar(
-                                  content: const Text("News 2 Detail"))),
-                        )
-                      ]),
-                  CollapsibleItem(
-                    text: 'News 3',
-                    icon: Icons.three_k,
-                    onPressed: () => setState(() => _headline = 'News 3'),
-                    onHold: () => ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: const Text("News 3"))),
-                  )
-                ]),
-            CollapsibleItem(
-              text: 'New News',
-              icon: Icons.account_balance,
-              onPressed: () => setState(() => _headline = 'New News'),
-              onHold: () => ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: const Text("New News"))),
-            ),
-          ]),
-      CollapsibleItem(
-        text: 'Face',
-        icon: Icons.face,
-        onPressed: () => setState(() => _headline = 'Face'),
-        onHold: () => ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: const Text("Face"))),
       ),
     ];
   }
@@ -189,33 +145,33 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return SafeArea(
-      child: CollapsibleSidebar(
-        isCollapsed: MediaQuery.of(context).size.width <= 800,
-        items: _items,
-        collapseOnBodyTap: false,
-        avatarImg: _avatarImg,
-        title: 'John Smith',
-        onTitleTap: () {
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Yay! Flutter Collapsible Sidebar!')));
-        },
-        body: _body(size, context),
-        backgroundColor: Colors.black,
-        selectedTextColor: Colors.limeAccent,
-        textStyle: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
-        titleStyle: TextStyle(
-            fontSize: 20,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold),
-        toggleTitleStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        sidebarBoxShadow: [
-        BoxShadow(
-            color: Colors.black12,
-            blurRadius: 20,
-            spreadRadius: 0.01,
-            offset: Offset(3, 3),
-          )
-        ],
+      child: Scaffold(
+        body: CollapsibleSidebar(
+          isCollapsed: MediaQuery.of(context).size.width <= 800,
+          items: _items,
+          collapseOnBodyTap: false,
+          title: 'P&A Jewerly',
+          onTitleTap: () {
+        
+          },
+          body: _body(size, context),
+          backgroundColor: Colors.black,
+          selectedTextColor: Colors.blueAccent,
+          textStyle: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
+          titleStyle: const TextStyle(
+              fontSize: 15,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold),
+          toggleTitleStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+          sidebarBoxShadow: const [
+          BoxShadow(
+              color: Colors.black12,
+              blurRadius: 20,
+              spreadRadius: 0.01,
+              offset: Offset(3, 3),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -224,24 +180,8 @@ class _HomeScreenState extends State<HomeScreen> {
     return Container(
       height: double.infinity,
       width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.redAccent,
-      ),
-      child: Center(
-        child: Transform.rotate(
-          angle: math.pi / 2,
-          child: Transform.translate(
-            offset: Offset(-size.height * 0.3, -size.width * 0.23),
-            child: Text(
-              _headline,
-              style: Theme.of(context).textTheme.displayLarge,
-              overflow: TextOverflow.visible,
-              softWrap: false,
-            ),
-          ),
-        ),
-      ),
+      margin: EdgeInsets.all(20),
+      child: _currentWidget,
     );
   }
 }
